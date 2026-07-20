cf42e565635e65fa15876dc2730aa1a76c930b28
thenikhilsingh <nikhilsingh26042004@gmail.com>
2026-07-20 07:31:47 +0530

    create context and routers on frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AuthProvider from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* Auth Routes */}
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      {/* Admin Routes */}
+      {/* App Routes */}
+      <Route path="/app" element={<App />}>
+        {/* <Route index element={<Home />}></Route> */}
+      </Route>
+    </>,
+  ),
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
